e0dbdc89fa6537a2dc38dc3cb29c1dc6a79f706d
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-05-22 17:44:57 +0530

    Pushed all file from local to remote repo

---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+
+const http = require("http");
+
+const url = require("url");
+
+/* -------------------------------------------------------------------------- */
+/*                                   FILES                                    */
+/* -------------------------------------------------------------------------- */
+
+/// BLOCKING, SYNCHRONOUS WAY
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado : ${textIn}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// console.log("File written!");
+
+/// NON-BLOCKING, ASYNCHRONOUS WAY
+
+// fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
+//   if (error) return console.log("ERROR 💥");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
+//       console.log(data3);
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2}\n${data3}`,
+//         "utf-8",
+//         (error) => {
+//           console.log("Your file has been written 🧑🏻‍🔧");
+//         }
+//       );
+//     });
+//   });
+// });
+
+// console.log("Will read file!");
+
+/* -------------------------------------------------------------------------- */
+/*                                   SERVER                                   */
+/* -------------------------------------------------------------------------- */
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
