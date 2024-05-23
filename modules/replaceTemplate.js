500fd854b91c2360ce81fbc28dd02f0fc2422a82
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-05-23 23:29:32 +0530

    Fixed the NUTRIENTS template

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -6,6 +6,7 @@ module.exports = (temp, product) => {
   output = output.replace(/{%FROM%}/g, product.from);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
   output = output.replace(/{%ID%}/g, product.id);
   if (!product.organic)
     output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
